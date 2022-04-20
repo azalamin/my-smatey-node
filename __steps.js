bcadef525d20ca4263965fba75685c73be37acf7
Al-amin Sheikh <azalamins@gmail.com>
2022-04-21 02:50:20 +0600

    added steps for node server setup

---
+++ __steps.js
@@ -0,0 +1,32 @@
+/* 
+* -----------------------------
+*  Basic server setup
+* -----------------------------
+* 1. create a folder (manually)
+* 2. open command line to that folder
+* 3. npm init
+* 4. npm install express
+* 5. create index.js
+* ------------------------------
+* For Express/node server
+* ------------------------------
+* 1. require express
+* 2. create app variable
+* 3. declare port
+* 4. set app.get('/')
+* 5. listen to port
+* 6. node index.js (in terminal)
+* 7. check your browser for that port
+* -----------------------------
+*  Get
+* -----------------------------
+* 1. cors middleware
+* 2. npm install cors
+* 3. require cors
+* 4. use cors (middleware)
+* -----------------------------
+*  Post Request
+* -----------------------------
+* 1. Get the path of posting data & give it as first parameter of fetch.
+* 2. set method, headers, body in fetch as object of fetch second parameter
+*/
